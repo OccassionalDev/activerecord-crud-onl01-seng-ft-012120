10113c2826012f62bb2e6a50d43cf39924c8d471
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-26 03:02:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -93,7 +93,9 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   
-  __
+  movie_to_destroy = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  
+  movie_to_destroy.destroy
 end
 
 def can_destroy_all_items_at_once

b982692b3879c5d931d5f9f25abc776b65450a0a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-26 02:57:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -77,8 +77,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  current_movie = Movie.find_by(title: "Wat?")
+  current_movie.update()
 end
 
 def can_update_multiple_items_at_once
